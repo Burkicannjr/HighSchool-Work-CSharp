c7a5839781b1b5b6052d7c1e8bb44f77e6da2b95
Yusuf Buğra Mahmut <ybmhmt92@gmail.com>
2026-03-03 22:06:40 +0300

    Add files via upload

---
+++ Yusuf Bugra APP8/Yusuf Buğra U11/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Yusuf_Buğra_U11
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(156, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Giriş";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(156, 158);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(127, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Ekran_görüntüsü_2024_08_22_164823;
+            pictureBox1.Location = new Point(113, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(197, 115);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(9, 161);
+            label1.Name = "label1";
+            label1.Size = new Size(141, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Kullanıcı Adı Giriniz:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkKhaki;
+            ClientSize = new Size(382, 280);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Kullanıcı Adı Sorgulama";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
