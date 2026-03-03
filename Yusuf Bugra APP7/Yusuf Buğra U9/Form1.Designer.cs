43697e6e5318cd6067188e1bcd0d0c546708ca01
Yusuf Buğra Mahmut <ybmhmt92@gmail.com>
2026-03-03 22:10:05 +0300

    Add files via upload

---
+++ Yusuf Bugra APP7/Yusuf Buğra U9/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Yusuf_Buğra_U9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(54, 65);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(766, 237);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Türkçe (40 soru)";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(28, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Matematik (40 soru)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(291, 23);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Doğru";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(418, 23);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Yanlış";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(559, 23);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Net";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(280, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(69, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(280, 118);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(69, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(408, 56);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(69, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(408, 118);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(69, 27);
+            textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(540, 56);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(69, 27);
+            textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(540, 118);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(69, 27);
+            textBox6.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Salmon;
+            ClientSize = new Size(1335, 537);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
